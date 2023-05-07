d1c488b2f5bc43d305b4dfa56bf891308b02aaac
juthalen <judith.alende@gmail.com>
2023-05-07 12:59:37 +0200

    ultimo cambio?

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','modulo1_modulo'],
+    'depends': ['base','modulo1'],
 
     # always loaded
     'data': [
