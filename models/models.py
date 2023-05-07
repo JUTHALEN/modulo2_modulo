2fd8e1494e18999be02f76a602c1b22906c01ece
juthalen <judith.alende@gmail.com>
2023-05-07 12:00:25 +0200

    modulo2 dependiendia de modulo1

---
+++ models/models.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class modulo2_provincias(models.Model):
+    _name = 'modulo2.provincias'
+    _description = 'Modelo de provincias'
+
+    name = fields.Char(string="Nombre de provincia")
+    description = fields.Text(string="Descripción")
+
+
+class modulo2_direccion(models.Model):
+    _name = 'modulo2.direccion'
+    _description = 'Modelo de dirección'
+
+    name = fields.Char(string="Dirección")
+    provincia_id = fields.Many2one(
+        comodel_name='modulo2.provincias', 
+        string="Provincia", 
+        ondelete='restrict')
+
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+ 
