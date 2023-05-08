cdd5f7b240288cb5d44a47ffca5cb6ade43ce4cd
juthalen <judith.alende@gmail.com>
2023-05-08 16:57:01 +0200

    Revert "Creación de un modulo para la gestion de una academia"
    This reverts commit 1cd2e04ab7a11bde46a4b950fc760da2eaca4207.

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "academia",
+    'name': "modulo2",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        gestión de clases de una academia
+        Long description of module's purpose
     """,
 
-    'author': "Judith Alende Martínez - 4847933",
-    'website': "",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,15 +20,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','modulo1'],
 
     # always loaded
     'data': [
-        #'security/groups.xml',
+        'security/groups.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
-        #'views/templates.xml',
-        #'demo/demo.xml',
+        'views/templates.xml',
+        'demo/demo.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
